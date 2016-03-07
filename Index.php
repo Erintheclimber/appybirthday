5063b67b077a5c2183e0f4220fde182b7272300a
Erin Reilly <erinreilly@erinreilly-macbookair.roam.corp.google.com>
2016-03-06 21:38:48 -0800

    installed twilio sdk, created basic text message

--- Index.php
+++ Index.php
@@ -1,4 +1,5 @@
 <?php
+require __DIR__ . "/vendor/twilio/sdk/Services/Twilio.php";
 echo "hello ";
 
 $x = 1;
@@ -8,4 +9,20 @@ if ( $x == 0 ) {
 } else {
 	echo "erin";
 }
+ 
+// set your AccountSid and AuthToken from www.twilio.com/user/account
+$AccountSid = "AC974663c5155b55248dbef845b173296d";
+$AuthToken = "9cab697236f95c29833a1309349fafc1";
+ 
+$client = new Services_Twilio($AccountSid, $AuthToken);
+ 
+$message = $client->account->messages->create(array(
+    "From" => "510-847-3180",
+    "To" => "510-847-3180",
+    "Body" => "Coding is fun!",
+));
+ 
+// Display a confirmation message on the screen
+echo "Sent message {$message->sid}";
+
 ?>
