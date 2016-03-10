d089425f1286a45fd131e1bcae0f8af1b365579b
Erin Reilly <erinreilly@erinreilly-macbookair.roam.corp.google.com>
2016-03-09 18:20:56 -0800

    built form to send phone numbers to our app; added error handling for our app

--- Index.php
+++ Index.php
@@ -1,7 +1,7 @@
 <?php
 require __DIR__ . "/vendor/twilio/sdk/Services/Twilio.php";
-echo "hello ";
-
+$phone = $_POST["phone"];
+$birthday = $_POST["birthday"];
 $x = 1;
 
 if ( $x == 0 ) {
@@ -16,13 +16,16 @@ $AuthToken = "9cab697236f95c29833a1309349fafc1";
  
 $client = new Services_Twilio($AccountSid, $AuthToken);
  
-$message = $client->account->messages->create(array(
-    "From" => "510-847-3180",
-    "To" => "510-847-3180",
-    "Body" => "Coding is fun!",
-));
- 
-// Display a confirmation message on the screen
-echo "Sent message {$message->sid}";
+try {
+	$message = $client->account->messages->create(array(
+		"From" => "15005550006",
+		"To" => $phone,
+		"Body" => "Happy birthday!",
+	));
+	// Display a confirmation message on the screen
+	echo "error: Sent message {$message->sid}<br>";
+ } catch (Exception $e) {
+ 	echo $e->getMessage();
+ }
 
 ?>
